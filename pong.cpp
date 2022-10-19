530ebd2f7a29b731c860030047cd3ba1af98d928
Sankethsss <57710402+Sankethsss@users.noreply.github.com>
2022-10-19 10:09:25 +0530

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -1,22 +1,25 @@
 // pong.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+//commented headers are used in windows
 
-#include <string>
-#include <windows.h>
+//#include "stdafx.h"
+
+#include <string.h>
+#include <ctype.h>
+//#include <cstring.h>
+//#include <windows.h>
 #include <iostream>
-#include <conio.h>
+//#include <conio.h>
 #include <sstream>
 #include <math.h>
-#include <gl\gl.h>
-#include <gl\glu.h>
-#include "GL/freeglut.h"
-#pragma comment(lib, "OpenGL32.lib")
-// keycodes
-#define VK_W 0x57
-#define VK_S 0x53
 
+//#include <gl\glu.h>
+//#include "GL/freeglut.h"
+#include <GL/glut.h>
+
+
+// window screen number
+int view=0;
 // window size and update rate (60 fps)
 int width = 500;
 int height = 200;
@@ -25,6 +28,7 @@ int interval = 1000 / 60;
 // score
 int score_left = 0;
 int score_right = 0;
+char str[30];
 
 // rackets in general
 int racket_width = 10;
@@ -45,19 +49,8 @@ float ball_pos_y = height / 2;
 float ball_dir_x = -1.0f;
 float ball_dir_y = 0.0f;
 int ball_size = 8;
-int ball_speed = 2;
+int ball_speed = 3;
 
-std::string int2str(int x) {
-    // converts int to string
-    std::stringstream ss;
-    ss << x;
-    return ss.str( );
-}
-
-void drawText(float x, float y, std::string text) {
-    glRasterPos2f(x, y);
-    glutBitmapString(GLUT_BITMAP_8_BY_13, (const unsigned char*)text.c_str());
-}
 
 void drawRect(float x, float y, float width, float height) {
     glBegin(GL_QUADS);
@@ -69,36 +62,94 @@ void drawRect(float x, float y, float width, float height) {
 }
 
 
+//to display bitmap char (strings)
+void display_string(int x, int y, char *string, int font)
+{
+    int len,i;
+	glRasterPos2f(x, y);
+    len = (int) strlen(string);
+    for (i = 0; i < len; i++) {
+    if(font==1)
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,string[i]);
+    if(font==2)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,string[i]);
+	}
+
+}
+
+
+void drawMenu(){
+	display_string(width/2-35,150,"PONG",1);
+	display_string(width/3+30,120,"Press Y to Play",2);
+	display_string(width/3+30,100,"Press H for Help",2);
+	display_string(width/3+30,80,"Press I for Info",2);
+	glutPostRedisplay();
+	
+}
+
+void drawHelp(){
+	display_string(width/2-35,150,"HELP",1);
+	display_string(30,120,"Press W/S to Move Left plank Up/Down Respectively",2);
+	display_string(30,100,"Press 8/5 to Move Right plank Up/Down Respectively",2);
+	display_string(30,80,"Points are awarded when ball exits at opponent's side",2);
+	display_string(30,60,"Press Q to pause game",2);
+	glutPostRedisplay();
+	
+}
+
+void drawInfo(){
+	display_string(width/2-55,150,"Developers",1);
+	display_string(width/3+10,120,"Sanketh (4VP19CS078)",2);
+	display_string(width/3+10,100,"Sandesh S. C.(4VP19CS078)",2);
+	glutPostRedisplay();
+	
+}
+
+
 void draw() {
     // clear (has to be done at the beginning)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+	if(view==0) drawMenu();   // Main menu window
 
+	else if(view==1) drawHelp();  //Help window
     
-    // draw rackets
-    drawRect(racket_left_x, racket_left_y, racket_width, racket_height);
-    drawRect(racket_right_x, racket_right_y, racket_width, racket_height);
+	else if(view==3) drawInfo();     //Developer info
 
-    // draw ball 
-     drawRect(ball_pos_x, ball_pos_y, ball_size, ball_size);
+	else if(view==2){
+		// draw rackets
+		drawRect(racket_left_x, racket_left_y, racket_width, racket_height);
+		drawRect(racket_right_x, racket_right_y, racket_width, racket_height);
 
-	 // draw score
-    drawText(width / 2 - 10, height - 15, int2str(score_left) + ":" + int2str(score_right));
+		// draw ball 
+		 drawRect(ball_pos_x, ball_pos_y, ball_size, ball_size);
 
+		 // draw score
+		 
+		 sprintf(str,"%d:%d",score_left,score_right);
+	
+		 display_string(width / 2 - 10, height - 15, str,2);
+
+	}
 
     // swap buffers (has to be done at the end)
     glutSwapBuffers();
 }
 
-void keyboard() {
+void keyboard(unsigned char b,int x,int y) {
     // left racket
-	//if
-    if (GetAsyncKeyState(VK_W)&& ((racket_left_y + racket_height)<=height)) racket_left_y += racket_speed;
-    if (GetAsyncKeyState(VK_S) && ( racket_left_y >=0)) racket_left_y -= racket_speed;
+    if ((b=='w'|| b=='W')&& ((racket_left_y + racket_height)<=height)) racket_left_y += racket_speed;
+    if ((b=='s'|| b=='S') && ( racket_left_y >=0)) racket_left_y -= racket_speed;
    
     // right racket
-    if (GetAsyncKeyState(VK_UP)&& ((racket_right_y + racket_height)<=height)) racket_right_y += racket_speed;
-    if (GetAsyncKeyState(VK_DOWN) && ( racket_right_y >=0)) racket_right_y -= racket_speed;
+    if ((b=='8') && ((racket_right_y + racket_height)<=height)) racket_right_y += racket_speed;
+    if ((b=='5') && ( racket_right_y >=0)) racket_right_y -= racket_speed;
+
+	// Navigation
+	if((b=='q'|| b=='Q')) view=0;     //mainmenu
+	if((b=='y'|| b=='Y')) view=2;     //game window
+	if((b=='h'|| b=='H')) view=1;     //help window
+	if((b=='i'|| b=='I')) view=3;     //Developer window
 }
 
 void vec2_norm(float& x, float &y) {
@@ -175,15 +226,16 @@ void updateBall() {
 }
 
 void update(int value) {
-   // input handling
-   keyboard();
-
+    
+   if(view==2){
    // update ball
    updateBall();
+   }
 
 
    // Call update() again in 'interval' milliseconds
    glutTimerFunc(interval, update, 0);
+   
 
    // Redisplay frame
    glutPostRedisplay();
@@ -200,15 +252,17 @@ void enable2D(int width, int height) {
 
 
 // program entry point
-int _tmain(int argc, char** argv) {
+int main(int argc, char** argv) {
     // initialize opengl (via glut)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(width, height);
     glutCreateWindow("Pong");
- 
+    
+    glutKeyboardFunc(keyboard);
 	// Register callback functions  
     glutDisplayFunc(draw);
+	
     glutTimerFunc(interval, update, 0);
 
     // setup scene to 2d mode and set draw color to white
